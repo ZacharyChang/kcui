883d328a2164a950f26c46b2baa33a3fd03865d9
Zachary <zacharychang.zc@gmail.com>
2019-03-17 23:58:22 +0800

    update: add pod log

--- main.go
+++ main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"log"
+	"os/exec"
+	"strings"
+
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
 
 func main() {
 	app := tview.NewApplication()
-	list := tview.NewList().ShowSecondaryText(false).
-		AddItem("List item 1", "", '1', nil).
-		AddItem("List item 2", "", '2', nil).
-		AddItem("List item 3", "", '3', nil).
-		AddItem("List item 4", "", '4', nil).
-		AddItem("Quit", "Press to exit", 'q', func() {
-			app.Stop()
-		})
-	list.SetBorder(true).SetTitle("Pod")
+	listView := tview.NewList().ShowSecondaryText(false)
+	// skip the column name
+	for i, podName := range getPodNames() {
+		if i > 0 {
+			listView.AddItem(podName, "", rune('a'+i-1), nil)
+		}
+	}
+	listView.SetBorder(true).SetTitle("Pod")
+	logView := tview.NewTextView()
+	logView.SetBorder(true).SetTitle("Log")
 	flex := tview.NewFlex().
-		AddItem(list, 0, 1, false).
-		AddItem(tview.NewBox().SetBorder(true).SetTitle("Log"), 0, 3, false)
+		AddItem(listView, 0, 1, false).
+		AddItem(logView, 0, 3, false)
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyESC {
 			app.Stop()
@@ -27,19 +33,58 @@ func main() {
 			app.Stop()
 		}
 		if event.Key() == tcell.KeyUp {
-			list.SetCurrentItem(list.GetCurrentItem() - 1)
+			newItem := listView.GetCurrentItem() - 1
+			if newItem == -1 {
+				newItem = listView.GetItemCount() - 1
+			}
+			podName, _ := listView.GetItemText(newItem)
+			listView.SetCurrentItem(newItem)
+			writePodLogs(logView, podName)
 		}
 		if event.Key() == tcell.KeyDown {
-			newItem := list.GetCurrentItem() + 1
-			if newItem < list.GetItemCount() {
-				list.SetCurrentItem(newItem)
-			} else {
-				list.SetCurrentItem(0)
+			newItem := listView.GetCurrentItem() + 1
+			if newItem == listView.GetItemCount() {
+				newItem = 0
 			}
+			podName, _ := listView.GetItemText(newItem)
+			listView.SetCurrentItem(newItem)
+			writePodLogs(logView, podName)
 		}
 		return event
 	})
 	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
 	}
+}
+
+func getPodNames() []string {
+	cmd := "kubectl get pods |awk '{print $1}'"
+	out, err := exec.Command("bash", "-c", cmd).CombinedOutput()
+	if err != nil {
+		log.Fatalf("cmd.Run() fail wtih %s\n", err)
+	}
+	pods, err := StringToLines(string(out))
+	if err != nil {
+		log.Fatalf("getPodNames() fail: %s\n", err)
+	}
+	return pods
+}
+
+func writePodLogs(target *tview.TextView, podName string) {
+	cmd := "kubectl logs " + podName + " --all-containers --tail 100"
+
+	out, err := exec.Command("bash", "-c", cmd).CombinedOutput()
+	if err != nil {
+		log.Fatalf("cmd.Run() fail wtih %s\n", err)
+	}
+	target.SetText(string(out))
+}
+
+func StringToLines(s string) (lines []string, err error) {
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	return
 }
