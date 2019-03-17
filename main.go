cba6073084b323216a59a75e04157a46ed49d9e7
Zachary <zacharychang.zc@gmail.com>
2019-03-17 15:48:30 +0800

    update: add tview

--- main.go
+++ main.go
@@ -1,41 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/jroimartin/gocui"
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
 )
 
 func main() {
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panicln(err)
-	}
-	defer g.Close()
-
-	g.SetManagerFunc(layout)
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
-}
-
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("KCUI", maxX/2-17, maxY/2, maxX/2+17, maxY/2+2); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
+	app := tview.NewApplication()
+	list := tview.NewList().ShowSecondaryText(false).
+		AddItem("List item 1", "", '1', nil).
+		AddItem("List item 2", "", '2', nil).
+		AddItem("List item 3", "", '3', nil).
+		AddItem("List item 4", "", '4', nil).
+		AddItem("Quit", "Press to exit", 'q', func() {
+			app.Stop()
+		})
+	list.SetBorder(true).SetTitle("Pod")
+	flex := tview.NewFlex().
+		AddItem(list, 0, 1, false).
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("Log"), 0, 3, false)
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyESC {
+			app.Stop()
 		}
-		fmt.Fprintln(v, "Kubernetes Console User Interface")
+		if event.Key() == tcell.KeyCtrlQ {
+			app.Stop()
+		}
+		if event.Key() == tcell.KeyUp {
+			list.SetCurrentItem(list.GetCurrentItem() - 1)
+		}
+		if event.Key() == tcell.KeyDown {
+			newItem := list.GetCurrentItem() + 1
+			if newItem < list.GetItemCount() {
+				list.SetCurrentItem(newItem)
+			} else {
+				list.SetCurrentItem(0)
+			}
+		}
+		return event
+	})
+	if err := app.SetRoot(flex, true).Run(); err != nil {
+		panic(err)
 	}
-	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
 }
