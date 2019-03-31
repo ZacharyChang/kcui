76c3bfca2dd484aca0891e863204a32e4dd0670b
ZacharyChang <zacharychang.zc@gmail.com>
2019-03-31 21:00:34 +0800

    add: log function comment

--- log/log.go
+++ log/log.go
@@ -38,44 +38,54 @@ func logf(level string, format string, v ...interface{}) {
 	golog.Print(currentTime(), level, fmt.Sprintf(format, v...))
 }
 
+// Fatal print logs like log.Print, but has a prefix with "[FATAL]"
 func Fatal(v ...interface{}) {
 	log(fatal, v...)
 	os.Exit(1)
 }
 
+// Fatalf print logs like log.Printf, but has a prefix with "[FATAL]"
 func Fatalf(format string, v ...interface{}) {
 	logf(fatal, format, v...)
 	os.Exit(1)
 }
 
+// Error print logs like log.Print, but has a prefix with "[ERROR]"
 func Error(v ...interface{}) {
 	log(error, v...)
 }
 
+// Errorf print logs like log.Printf, but has a prefix with "[ERROR]"
 func Errorf(format string, v ...interface{}) {
 	logf(error, format, v...)
 }
 
+// Warn print logs like log.Print, but has a prefix with "[WARN]"
 func Warn(v ...interface{}) {
 	log(warn, v...)
 }
 
+// Warnf print logs like log.Printf, but has a prefix with "[WARN]"
 func Warnf(foramt string, v ...interface{}) {
 	logf(warn, foramt, v...)
 }
 
+// Info print logs like log.Print, but has a prefix with "[INFO]"
 func Info(v ...interface{}) {
 	log(info, v...)
 }
 
+// Infof print logs like log.Printf, but has a prefix with "[INFO]"
 func Infof(format string, v ...interface{}) {
 	logf(info, format, v...)
 }
 
+// Debug print logs like log.Print, but has a prefix with "[DEBUG]"
 func Debug(v ...interface{}) {
 	log(debug, v...)
 }
 
+// Debugf print logs like log.Printf, but has a prefix with "[DEBUG]"
 func Debugf(format string, v ...interface{}) {
 	logf(debug, format, v...)
 }
